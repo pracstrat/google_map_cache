05ce4a7b9d4011d6bdc6b22c21520e7632d14e58
Philip Hallstrom <philip@51shepherd.com>
2013-12-09 14:21:00 +0800

    make the secret not secret

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoogleMapCache::Application.config.secret_key_base = '516d5de2a8400dabb75ad84c5fa5ce921f140380bd8d2e2705a4869a84fb64ff15d6bb5c07deef56ba52ae5c845de90fb4b8b7bb447e6e3d03ca26d1032ca686'
