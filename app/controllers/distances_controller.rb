6206fa1747876915b6ebd3c118a2eb38c8bd0ebd
Philip Hallstrom <philip@practicalstrategies.com>
2013-12-10 12:18:30 +0800

    not found

--- app/controllers/distances_controller.rb
+++ app/controllers/distances_controller.rb
@@ -1,21 +1,30 @@
 class DistancesController < ApplicationController
   def calculate
     distance = Distance.where(origin: params[:from], destination: params[:to]).first
+    not_found = false
     
     unless distance.present?
       d = Distance.distance(params[:from], params[:to])
-      case d
-      when 0
+      if d == 0
         render nothing: true, status: :not_found
         return
-      when -1
-        render json: -1
-        return
+      end
+        
+      if d == -1
+        not_found = true
       else
         distance = Distance.create!(origin: params[:from], destination: params[:to], distance: d)
-        render json: distance.distance
-        return
       end
+    end
+    
+    respond_to do |format|
+      format.json {
+        if not_found
+          render json: -1
+        else
+          render json: distance.distance
+        end
+      }
     end
   end
 end
